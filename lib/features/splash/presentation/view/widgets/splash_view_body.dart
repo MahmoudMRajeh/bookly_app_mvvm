309bc809eeb79e24f362a6960ba33c7f7b9b2dce
MahmoudMRajeh <mahmoudmrajeh@gmail.com>
2023-10-02 17:30:22 +0300

    refactor initialization code of sliding animation

--- lib/features/splash/presentation/view/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/view/widgets/splash_view_body.dart
@@ -16,22 +16,15 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(
-        seconds: 1,
-      ),
-    );
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 20), end: Offset.zero)
-            .animate(animationController);
-            animationController.forward();
+    initSlidingAnimation();
   }
+
   @override
   void dispose() {
     super.dispose();
     animationController.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -46,5 +39,17 @@ class _SplashViewBodyState extends State<SplashViewBody>
       ],
     );
   }
-}
 
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 1,
+      ),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 20), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+}
