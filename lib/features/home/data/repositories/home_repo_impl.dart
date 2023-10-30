6fc22fb7b841c5177cf0c3b59789ae63440917d0
MahmoudMRajeh <mahmoudmrajeh@gmail.com>
2023-10-30 19:32:33 +0200

    create server failure from dio error

--- lib/features/home/data/repositories/home_repo_impl.dart
+++ lib/features/home/data/repositories/home_repo_impl.dart
@@ -3,11 +3,14 @@ import 'package:alarm_app/core/utils/dio_servieces.dart';
 import 'package:alarm_app/features/home/data/models/book_model/book_model.dart';
 import 'package:alarm_app/features/home/data/repositories/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
 class HomeRepositoryImpl implements HomeRepository {
   final DioServices dioServices;
 
   HomeRepositoryImpl({required this.dioServices});
+
   @override
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
     try {
@@ -20,7 +23,12 @@ class HomeRepositoryImpl implements HomeRepository {
       }
       return Right(books);
     } on Exception catch (error) {
-      return Left(ServerFailure(errMsg: 'Failed to fetch data'));
+      if (error is DioException) {
+        return Left(ServerFailure.fromDioError(error));
+      }
+      debugPrint(" error is ${error.toString()}");
+      return const Left(
+          ServerFailure(errMsg: "Opps There was an Error, Please try again !"));
     }
   }
 
