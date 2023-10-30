22896e207fb19129306f08ff162c9872775c79f5
MahmoudMRajeh <mahmoudmrajeh@gmail.com>
2023-10-30 19:36:44 +0200

    implement fetch featured books in home repo

--- lib/features/home/data/repositories/home_repo_impl.dart
+++ lib/features/home/data/repositories/home_repo_impl.dart
@@ -33,7 +33,23 @@ class HomeRepositoryImpl implements HomeRepository {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await dioServices.get(
+          endPoints:
+              "volumes?Filtering=free-ebooks&q=subject:programming");
+      List<BookModel> books = [];
+      for (var i in data["items"]) {
+        books.add(BookModel.fromJson(i));
+      }
+      return Right(books);
+    } on Exception catch (error) {
+      if (error is DioException) {
+        return Left(ServerFailure.fromDioError(error));
+      }
+      debugPrint(" error is ${error.toString()}");
+      return const Left(
+          ServerFailure(errMsg: "Opps There was an Error, Please try again !"));
+    }
   }
 }
